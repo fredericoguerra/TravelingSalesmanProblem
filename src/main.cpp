247323fc326644f23fcb6d0649aa845849c42922
fredericoguerra <frederico.souza.guerra@gmail.com>
2022-09-26 23:53:43 -0300

    ILS - Distance function for initial solution implemented

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,21 @@ void printData() {
   }
 }
 
-vector<int> initialSolution(){
+struct initialSolution {
+  vector<int> route;
+  int distance;
+};
+
+initialSolution genInitialSolution(){
   vector<int> initialRoute(dimension);
+  int routeSize=0;
+
   for (int i = 0; i < dimension; i++){
     initialRoute[i] = i;
   }
 
-  for (int l1 = 2; l1 < dimension-1; l1++){
-    nearestRoute = distanceMatrix[initialRoute[l1-1]][initialRoute[l1]];
+  for (int l1 = 1; l1 < dimension-1; l1++){
+    nearestRoute = distanceMatrix[initialRoute[l1-1]+1][initialRoute[l1]+1];
     nearestNeighbor = initialRoute[l1];
     //cout << endl << endl << "L1: " << nearestNeighbor << " Distance from " << initialRoute[l1-1] << ": "<< nearestRoute << endl;
 
@@ -43,9 +50,9 @@ vector<int> initialSolution(){
   //}
 
     for (int l2 = 1; l2 < notVisitedNeighbors.size(); l2++){
-      //cout << "L2: " << notVisitedNeighbors[l2] << " | " << "Distance from " << initialRoute[l1-1] << ": " << distanceMatrix[initialRoute[l1-1]][notVisitedNeighbors[l2]] << "  ";
-      if((initialRoute[l1-1] != notVisitedNeighbors[l2]) && (distanceMatrix[initialRoute[l1-1]][notVisitedNeighbors[l2]] < nearestRoute)){
-        nearestRoute = distanceMatrix[initialRoute[l1-1]][notVisitedNeighbors[l2]];
+      //cout << "L2: " << notVisitedNeighbors[l2] << " | " << "Distance from " << initialRoute[l1-1] << ": " << distanceMatrix[initialRoute[l1-1]+1][notVisitedNeighbors[l2]+1] << "  ";
+      if((initialRoute[l1-1] != notVisitedNeighbors[l2]) && (distanceMatrix[initialRoute[l1-1]+1][notVisitedNeighbors[l2]+1] < nearestRoute)){
+        nearestRoute = distanceMatrix[initialRoute[l1-1]+1][notVisitedNeighbors[l2]+1];
         nearestNeighbor = notVisitedNeighbors[l2];
         }
       }
@@ -53,37 +60,43 @@ vector<int> initialSolution(){
       //cout << "Neighbor: " << nearestNeighbor << "Distance: " << nearestRoute << endl;
       //cout << endl <<"Before swap:" << endl;
       //for (int i = 0; i < dimension; i++){
-        //cout << initialRoute[i] << " ";
+      //  cout << initialRoute[i] << " ";
       //}
       int index = find(notVisitedNeighbors.begin(), notVisitedNeighbors.end(), nearestNeighbor) - notVisitedNeighbors.begin();
       index += l1;
 
       swap(initialRoute[l1], initialRoute[index]);
       //cout << endl <<"Swaped:" << endl;
-      //for (int k = 0; k < dimension; k++){
-      //  cout << initialRoute[k] << " ";
-      //}
+      for (int k = 0; k < dimension; k++){
+        //cout << initialRoute[k] << " ";
+      }
     }
   }
-  return initialRoute;
-}
 
+  initialRoute.push_back(0);
+
+  for (int i=1; i < dimension +1; i++){
+    //cout
+    cout << distanceMatrix[initialRoute[i-1]+1][initialRoute[i]+1] << " ";
+    routeSize += distanceMatrix[initialRoute[i-1]+1][initialRoute[i]+1];
+  }
+
+  return initialSolution{initialRoute, routeSize};
+}
 
 int main(int argc, char** argv){
   readData(argc, argv, &dimension, &distanceMatrix);
-  //cout << endl << "distanceMatrix: " << distanceMatrix[1][5] << endl;
-    
-  printData();
 
+  printData();
   //Generates Initial Solution
-  vector<int> baselineSolution;
-  baselineSolution = initialSolution();
-  baselineSolution.push_back(0);
+  initialSolution baselineSolution;
+  baselineSolution = genInitialSolution();
   cout << endl << "InitialRoute: ";
   for (int i = 0; i < dimension+1; i++){
-    baselineSolution[i] += 1;
-    cout << baselineSolution[i] << ' ';
+  baselineSolution.route[i] += 1;
+  cout << baselineSolution.route[i] << " ";
   }
+  cout << endl << "Distance: " << baselineSolution.distance << endl;
 
     return 0;
 }
